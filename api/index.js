00420bf2231ce273a5d948e243d3677166129eeb
Igor Milosavljevic <migor@live.co.uk>
2023-11-29 18:29:37 +0000

    connected database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,18 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+//connect to database
+mongoose
+  .connect(process.env.MONGO_DATABASE)
+  .then(() => {
+    console.log('Connected to MongoDB!');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
